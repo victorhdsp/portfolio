c36d9bdbaeebe77a4597459dea459cc16cacf3f6
victorhdsp <victorhugods.pereira@gmail.com>
2025-06-04 16:53:08 -0300

    fix: :recycle: Set carrosel in full width

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import HomeHero from "@/components/organisms/Hero";
 import HomeContact from "@/components/organisms/Contact/portfolio";
 import Footer from "@/components/organisms/Footer";
 import HomeProjects from "@/components/organisms/Projects";
+import css from "./home.module.scss"
 
 export default function Home() {
   return (<>
-    <main>
+    <main className={css.root}>
       <HomeHero />
       <HomeProjects />
       <HomeContact />
